179bf6ea0b4bf359bea82cf03a3470aaacb9430d
Soumee Mukherjee <52605586+SOUMEE2000@users.noreply.github.com>
2021-06-08 23:45:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,19 @@
-from flask import Flask
-from flask import render_template
+import requests
+import json
 
+from flask import Flask, session, render_template, request, redirect, url_for
+from flask_session import Session
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+
+# Configure session to use filesystem
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+# Set up database
+engine = create_engine("URL for database")
+db = scoped_session(sessionmaker(bind=engine))
 
 
 app = Flask(__name__)
@@ -12,12 +25,12 @@ def index():
 
 @app.route('/signup')
 def signup():
-    return render_template('signup.html')
+    return render_template('entry.html')
 
 
 
 @app.route('/questionare')
-def qustionare():
+def questionare():
     return render_template('questionare.html')
 
 
