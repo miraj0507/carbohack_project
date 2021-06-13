d671827fa9ea479fd710279e20f2c68dfee53d86
Aunomitra Ghosh <aunomitra.ghosh1999@gmail.com>
2021-06-14 04:01:29 +0530

    Database added and connected

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ from authlib.integrations.flask_client import OAuth
 import os, sys
 sys.path.append(".")
 from oauth_user import Google, Facebook, Linkedin, Twitter
+from databse import Database_Soumee
 
 
 app = Flask(__name__)
@@ -22,21 +23,31 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
+#session={}
+#session["message_code"]=1
+
 # Set up database
 #engine = create_engine("URL for database")
 #db = scoped_session(sessionmaker(bind=engine))
 
+db_S = Database_Soumee()
 
+
+#*********************************************** OUTSIDER VIEW /START ***************************************************************
 
 # Index page
 @app.route('/')
 def index():
+    if 'user' in session:
+        return redirect('/MyAccount')
     return render_template('index.html')
 
 
 @app.route('/entry_signin')
 @app.route('/entry_signup')
 def entry():
+    if 'user' in session:
+        return redirect('/MyAccount')
     return render_template('entry.html')
 
 #*************************************************************************
@@ -124,45 +135,63 @@ def auth_twitter():
 # MANUAL INPUT USER DATA /START
 @app.route('/processing_signup', methods=['POST'])
 def processing_signup():
-    user_info = request.json 
-    '''
-    user_info['firstname'] = request.form['firstname']
-    user_info['lastname'] = request.form['lastname']
-    user_info['email'] = request.form['email']
-    user_info['password'] = request.form['password']
-    user_info['location'] = request.form['location']
-    '''
+    if 'user' in session:
+        return redirect('/MyAccount')
+    user_info = request.json
+    print(user_info) 
+    db_S_respond=db_S.write_user_table(user_info)
     print(user_info)
-    return jsonify(resp="Correct")
+    return jsonify(resp1="Correct", resp2=db_S_respond)
 
 @app.route('/processing_signin', methods=['POST'])
 def processing_signin():
+    if 'user' in session:
+        return redirect('/MyAccount')
     user_info = request.json
     #user_info['email'] = request.form['email']
     #user_info['password'] = request.form['password']
+    if db_S.check_user_table(user_info):
+        if db_S.check_user_input_table(user_info):
+            return jsonify(resp1="Correct", resp2='/MyAccount')
+        else:
+            return jsonify(resp1="Correct", resp2="/questionare")
     print(user_info)
-    return jsonify(resp="Correct")
+    return jsonify(resp1="Incorrect", resp2='You are not in database, Please Sign Up First')
 
 # /END
 #**************************************************************************
 
+#************************************************** OUTSIDER VIEW /END **************************************************************
 
 # Logging in to get into the Questionaire page
 @app.route('/questionare')
-def login():
+def questionare():
+    if not ('user' in session):
+        return redirect('/')
     # Querying for relevant user details while logging an user in.
     # if match is found:
     # return render_template("questionaire.html")
     # else:
-    return render_template("entry.html")
+    return render_template("questionare.html")
 
 
 # User's account page. Left to be done
 @app.route('/MyAccount')
 def My_Account():
+    if not ('user' in session):
+        return redirect('/')
 #    Details of the parameters taken in from the questionaire. Stored in the database. Graph is plotted. Things to be calculated according to formulae present here.
 #    https://docs.google.com/document/d/1qZepM5Bbe13qaWUCraEf1Hmmb-otN7MImFnaBgSw44w/edit?ts=60bf9810
-    return render_template('dashboard.html') 
+    return render_template('dashboard.html')
+
+@app.route("/logout")
+def logout():
+    if not ('user' in session):
+        return redirect('/')
+    session.pop('username', None)
+    db_S.gbl_email=''
+    return redirect('/')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
