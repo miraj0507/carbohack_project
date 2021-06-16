b807d70676688cd50bf66ba6e4553c8033bc4884
the_guy_with_a_bun <aunomitra.ghosh1999@gmail.com>
2021-06-16 19:35:50 +0530

    Update entry.js

--- static/js/entry.js
+++ static/js/entry.js
@@ -10,7 +10,96 @@ sign_in_btn.addEventListener("click", () => {
   container.classList.remove("sign-up-mode");
 });
 
+$(document).ready(function() {
+
   if ((window.location.href).includes("signup")){
+    container.classList.add("sign-up-mode");
+    }
+
+  $('#signup').click(function(e) {
+    var user_info = {
+              firstname: $("#firstname").val(),
+              lastname: $("#lastname").val(),
+              email: $("#email-signup").val(),
+              password: $("#password-signup").val(),
+              location: $("#location").val()
+          }
+
+
+      $.ajax({
+          url: '/processing_signup',
+          type: "POST",
+          data : user_info,
+          //data: JSON.stringify(user_info),
+          //dataType: "json",
+          //contentType: 'application/json',
+          /*success: function(response) {
+            if (response.resp1 === 'Correct') {
+              if (response.resp2 === 'Registered'){
+                alert('Great!!! You are signed up .... Please sign in to access your account. ');
+                container.classList.remove("sign-up-mode");
+                }
+              else{
+                alert(response.resp2)
+
+              }
+
+            }
+            else {
+              alert("Server did not respond");
+            }
+          },*/
+          error: function(){
+            alert("server side error");
+          }
+      })
+      .done(function(data) {
+        alert(data.resp)
+      });
+      
+      e.preventDefault();
+
+  });
+
+  $('#signin').click(function(e) {
+    var user_signin={
+              email: $("#email-signin").val(),
+              password: $("#password-signin").val(),
+          }
+      $.ajax({
+          type: "POST",
+          url: '/processing_signin',
+          data: user_signin,
+          //data: JSON.stringify(user_signin),
+          //dataType: "json",
+          //contentType: 'application/json',
+          /*success: function(response)
+          {
+              if (response.resp1 === 'Correct') {
+                //console.log("done")
+                window.location.href = response.resp2; 
+              }
+              else {
+                  alert(response.resp2);
+              }
+          },*/
+          error: function(){
+            alert("server side error");
+          }
+      })
+      .done(function(data) {
+        if (data.resp === "Correct"){
+        window.location = '/MyAccount';}
+      });
+       e.preventDefault();
+  });
+
+  
+
+});
+
+
+ /* if ((window.location.href).includes("signup")){
   container.classList.add("sign-up-mode");
   }
 
@@ -82,5 +171,5 @@ sign_in_btn.addEventListener("click", () => {
       });
 
   });
-
+*/
   
